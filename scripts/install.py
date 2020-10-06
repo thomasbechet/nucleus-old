e5cb4de82d7f007046be948cac9793998fdfa297
Thomas <thomas.bechet.pro@gmail.com>
2020-10-06 22:22:00 +0200

    package installation

---
+++ scripts/install.py
@@ -0,0 +1,4 @@
+import subprocess
+
+subprocess.Popen('mkdir -p bin/', cwd='../build/').wait()
+subprocess.Popen('cp -r ../package/* bin/', cwd='../build/').wait()
--- scripts/run.py
+++ scripts/run.py
@@ -3,4 +3,4 @@ import time
 
 subprocess.Popen('mingw32-make install', cwd='../build/').wait()
 time.sleep(0.5)
-subprocess.Popen('../bin/nucleus_test', cwd='../bin/').wait()
+subprocess.Popen('bin/nucleus_test', cwd='../build/').wait()
