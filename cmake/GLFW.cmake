78d9bd1fe1bd3da8abb263e17ae602297d694a6f
thomas.bechet <thomas.bechet@univ-lyon1.fr>
2022-06-16 17:41:17 +0200

    fetch content of GLFW

--- cmake/GLFW.cmake
+++ cmake/GLFW.cmake
@@ -1,2 +1,18 @@
-include(ExternalProject)
+include(FetchContent)
 
+FetchContent_Declare(
+    glfw
+    GIT_REPOSITORY https://github.com/glfw/glfw
+)
+FetchContent_GetProperties(glfw)
+
+if(NOT glfw_POPULATED)
+    FetchContent_Populate(glfw)
+
+    set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+    set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
+
+    add_subdirectory(${glfw_SOURCE_DIR} ${glfw_BINARY_DIR})
+endif()
